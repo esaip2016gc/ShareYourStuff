089a0cd5249f9a181905c887407a37cde2ab7ee7
guillaume Belouin <applichic@gmail.com>
2017-06-28 12:02:33 +0200

    Init project

---
+++ src/main/java/home/HomeServlet.java
@@ -0,0 +1,22 @@
+package home;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by guillaumebelouin on 28/06/2017.
+ */
+@WebServlet("/")
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/index.jsp");
+        requestDispatcher.forward(req, resp);
+    }
+}
